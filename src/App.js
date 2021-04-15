eb9910fa270f6c7e3493abf37bbe6efcda2ec852
Peter Arthur <peterarthur@Peters-MBP.cust.communityfibre.co.uk>
2021-04-15 15:45:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=5e2c1492005767da5d9504070ca2b6a7`
+        )
+        .then((response) => {
+          setData(response);
+          console.log(response);
+        });
+    });
+  }, []);
+
+  return <div className="App"></div>;
+};
+
+export default App;
