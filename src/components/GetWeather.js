3550e1f9ade0f00af143450d404542206c040aab
Peter Arthur <peterarthur@Peters-MBP.cust.communityfibre.co.uk>
2021-04-22 16:58:21 +0100

    commit message

--- src/components/GetWeather.js
+++ src/components/GetWeather.js
@@ -16,10 +16,10 @@ const GetWeather = ({ weatherData }) => {
   };
 
   const convertToCelsius = function (temperature) {
-    return (temperature - 273.15).toFixed(2);
+    return (temperature - 273.15).toFixed(0);
   };
   const convertToFahrenheit = function (temperature) {
-    return ((temperature - 273.15) * (9 / 5) + 32).toFixed(2);
+    return ((temperature - 273.15) * (9 / 5) + 32).toFixed(0);
   };
 
   useEffect(() => {
@@ -31,60 +31,111 @@ const GetWeather = ({ weatherData }) => {
     localStorage.setItem("isCelsius", JSON.stringify(isCelsius));
   });
 
+  // https://static.thenounproject.com/png/1057070-200.png
+
   return (
     <div>
-      <h1>Weather Forecast </h1>
-      <button className="button" onClick={convert}>
-        Convert to {isCelsius ? "F" : "C"}
-      </button>
-      <img
-        src={`http://openweathermap.org/img/wn/${iconCode}@2x.png`}
-        alt="Weather description"
-      />
-      <div className="container-1">
-        <div className="Box-1">
-          <p>Weather: </p>
-          <p>{actualData.weather[0].main} </p>
+      <div className="left-side">
+        <div className="box-1">
+          <button className="button" onClick={convert}>
+            Convert to {isCelsius ? "F" : "C"}
+          </button>
         </div>
         <div className="box-2">
-          <p>Description:</p>
-          <p> {actualData.weather[0].description}</p>{" "}
+          <img
+            src={`http://openweathermap.org/img/wn/${iconCode}@2x.png`}
+            alt="Weather description"
+          />
         </div>
-        <div className="Box-3">
-          <p>
-            Current Temp:{" "}
+        <div className="box-3">
+          <p>Weather </p>
+          <h1>{actualData.weather[0].main} </h1>
+        </div>
+        <div className="box-4">
+          <p>Description</p>
+          <h1> {actualData.weather[0].description}</h1>{" "}
+        </div>
+        <div className="box-5">
+          <p>Current Temp </p>
+          <h1>
             {isCelsius
               ? convertToCelsius(mainData.temp)
               : convertToFahrenheit(mainData.temp)}
-          </p>
-          <p>
-            Feels Like:{" "}
+          </h1>
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="box-6">
+          <div className="practice">
+            <img
+              src="https://orioni.co/nmedia/png/humidity-4401.png"
+              alt="Humidity-icon"
+            />
+          </div>
+          <p>Humidity</p>
+          <h1> {mainData.humidity}%</h1>
+        </div>
+        <div className="box-7">
+          <div className="practice">
+            <img
+              src="https://img.icons8.com/ios/452/atmospheric-pressure.png"
+              alt="air-pressure-icon"
+            />
+          </div>
+          <p>Atmospheric Pressure</p>
+          <h1>
+            {temperature.pressure} {""}hPa
+          </h1>
+        </div>
+        <div className="box-8">
+          <div className="practice">
+            <img
+              src={`https://static.thenounproject.com/png/1057070-200.png 
+              `}
+              alt="Wind-icon"
+            />
+
+            {/* https://static.thenounproject.com/png/1057070-200.png */}
+          </div>
+          <p>Wind Speed </p>
+          <h1>
+            {dataWind.speed} {""} m/s
+          </h1>
+        </div>
+      </div>
+      <div className="clr"></div>
+      <div className="bottom">
+        <div className="box-9">
+          <p>Current Temp </p>
+          <h1>
+            {isCelsius
+              ? convertToCelsius(mainData.temp)
+              : convertToFahrenheit(mainData.temp)}
+          </h1>
+        </div>
+        <div className="box-10">
+          <p>Feels Like </p>
+          <h1>
             {isCelsius
               ? convertToCelsius(mainData.feels_like)
               : convertToFahrenheit(mainData.feels_like)}
-          </p>
+          </h1>
         </div>
-        <div className="Box-4">
-          <p>
-            Min Temp:{" "}
+        <div className="box-11">
+          <p>Min Temp </p>
+          <h1>
             {isCelsius
               ? convertToCelsius(mainData.temp_min)
               : convertToFahrenheit(mainData.temp_min)}{" "}
-          </p>
-          <p>
-            Max Temp:{" "}
+          </h1>
+        </div>
+        <div className="box-12">
+          <p>Max Temp </p>
+          <h1>
             {isCelsius
               ? convertToCelsius(mainData.temp_max)
               : convertToFahrenheit(mainData.temp_max)}
-          </p>
-        </div>
-        <div className="Box-5">
-          <p>Wind Speed: </p>
-          <p>{dataWind.speed}m/s</p>
-        </div>
-        <div className="Box-6">
-          <p>Humidity:</p>
-          <p> {mainData.humidity}%</p>
+          </h1>
         </div>
       </div>
     </div>
