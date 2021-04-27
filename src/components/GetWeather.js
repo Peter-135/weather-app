76e688888a4474362c80974541212cf25a835361
Peter Arthur <peterarthur@Peters-MBP.cust.communityfibre.co.uk>
2021-04-27 18:14:10 +0100

    commit message

--- src/components/GetWeather.js
+++ src/components/GetWeather.js
@@ -31,26 +31,55 @@ const GetWeather = ({ weatherData }) => {
     localStorage.setItem("isCelsius", JSON.stringify(isCelsius));
   });
 
-  // https://static.thenounproject.com/png/1057070-200.png
-
   /*   https://images.unsplash.com/photo-1534358594138-6955f589fa24?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGFyayUyMGNsb3VkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80  CLOUDY */
 
   /* https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg beautiful SUNSET */
 
   /* https://ak.picdn.net/shutterstock/videos/17816047/thumb/1.jpg  RAINY*/
 
-  // if ({actualData.weather[0].description}.includes("clear")) {
-  //   return url("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg")
-  //  }
-  //   if ({actualData.weather[0].description}.includes("rain")) {
-  //   return url("https://ak.picdn.net/shutterstock/videos/17816047/thumb/1.jpg")
-  // }
-  // if ({actualData.weather[0].description}.includes("cloud")) {
-  //   return url("https://ak.picdn.net/shutterstock/videos/17816047/thumb/1.jpg")
-  // }
+  const weatherImage = function () {
+    const weatherCondition = actualData.weather[0].description;
+    if (weatherCondition.includes("cloud")) {
+      return "cloudyWeather";
+    } else if (weatherCondition.includes("clear")) {
+      return "clearWeather";
+    } else if (weatherCondition.includes("rainy")) {
+      return "rainyWeather";
+    }
+  };
+
+  const getWeatherIcon = (iconCode) => {
+    let iconUrl = null;
+    if (iconCode == "02d" || iconCode == "03d" || iconCode == "04d") {
+      iconUrl =
+        "https://cdn2.iconfinder.com/data/icons/spring-flat-11/272/spring-cloud-weather-database-network-upload-download-256.png";
+    } else if (iconCode == "01d") {
+      iconUrl =
+        " https: //icons.iconarchive.com/icons/oxygen-icons.org/oxygen/256/Status-weather-clear-icon.png";
+    } else if (iconCode == "09d" || iconCode == "10d") {
+      iconUrl =
+        "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather05-512.png";
+    }
+
+    return iconUrl;
+  };
+
+  //cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather01-512.png
+
+  // https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather05-512.png
+
+  // RAINY ICON
+
+  // https: //icons.iconarchive.com/icons/oxygen-icons.org/oxygen/256/Status-weather-clear-icon.png
+
+  //  SUNNY ICON
+
+  // https://cdn2.iconfinder.com/data/icons/spring-flat-11/272/spring-cloud-weather-database-network-upload-download-256.png
+
+  // THE ABOVE LINK IS CLOUDY ICON I WILL BE USING, PUT IT BACK WHEN I'M DONE!!!
 
   return (
-    <div>
+    <div className={`background ${weatherImage()}`}>
       <div className="left-side">
         <div className="box-1">
           <button className="button" onClick={convert}>
@@ -58,10 +87,7 @@ const GetWeather = ({ weatherData }) => {
           </button>
         </div>
         <div className="box-2">
-          <img
-            src={`http://openweathermap.org/img/wn/${iconCode}@2x.png`}
-            alt="Weather description"
-          />
+          <img src={`${getWeatherIcon(iconCode)}`} alt="Weather description" />
         </div>
         <div className="box-3">
           <p>Weather </p>
@@ -73,11 +99,11 @@ const GetWeather = ({ weatherData }) => {
         </div>
         <div className="box-5">
           <p>Current Temp </p>
-          <h1>
+          <h2>
             {isCelsius
               ? convertToCelsius(mainData.temp)
               : convertToFahrenheit(mainData.temp)}
-          </h1>
+          </h2>
         </div>
       </div>
       <div className="right-side">
@@ -103,7 +129,7 @@ const GetWeather = ({ weatherData }) => {
                 alt="air-pressure-icon"
               />
             </div>
-            Atmospheric Pressure
+            Air Pressure
           </p>
           <h1>
             {temperature.pressure} {""}hPa
@@ -166,3 +192,7 @@ const GetWeather = ({ weatherData }) => {
 };
 
 export default GetWeather;
+
+{
+  /* <img src={`http://openweathermap.org/img/wn/${iconCode}@2x.png`} */
+}
